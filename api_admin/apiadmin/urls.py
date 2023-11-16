61cf43b6428a90d370cbc5b140db9fa72b7fd480
CarlosDaniel0293 <130116193+CarlosDaniel0293@users.noreply.github.com>
2023-11-16 01:35:24 -0500

    Delete api_admin directory

--- api_admin/apiadmin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AdminListCreateView, AdminDetailView, AdminLoginView, AdminLogoutView
-
-urlpatterns = [
-    path('admins/', AdminListCreateView.as_view(), name='admin-list-create'),
-    path('admins/<int:pk>/', AdminDetailView.as_view(), name='admin-detail'),
-    path('login/', AdminLoginView.as_view(), name='admin-login'),
-    path('logout/', AdminLogoutView.as_view(), name='admin-logout'),
-]

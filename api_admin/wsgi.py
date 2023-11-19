34c37271b0a69fba307b397dc78a66c18cc864f8
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-11-19 14:32:24 -0500

    cambios

--- api_admin/wsgi.py
+++ api_admin/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for api_admin project.
 
@@ -15,21 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_admin.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for api_admin project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_admin.settings')
-
-application = get_wsgi_application()
->>>>>>> c96de7ee265198347df3f9c5d11e523b4645620e

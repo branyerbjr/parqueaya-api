24a49a55c4b97a34db57a6e02bcc8d7a6f0ed8db
branyerbjr <branyerbjr@gmail.com>
2023-11-24 08:08:12 -0500

    api services es whatsapp y telegram

--- api_admin/urls.py
+++ api_admin/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('api/admin/', include('apiadmin.urls')),
     path('api/user/', include('apiuser.urls')),
     path('api/servicios/', include('servicios.urls')),
-    path('api/auto', include('apiauto.urls'))
+    path('api/auto', include('apiauto.urls')),
+    path('api/service', include('apiservices.urls')),
+
 ]

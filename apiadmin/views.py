0b4eee45911ece855bc62b05f1bc5c1c7a2fb439
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-12-14 18:08:19 -0500

    cambio aute

--- apiadmin/views.py
+++ apiadmin/views.py
@@ -3,10 +3,11 @@ from rest_framework import generics, permissions, status
 from .models import Admin, Usuario
 from .serializers import AdminSerializer, UsuarioSerializer, UsuarioLoginSerializer
 from rest_framework.response import Response
-from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
 
 class UsuarioListView(generics.ListAPIView):
@@ -27,33 +28,44 @@ class InicioSesion(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UsuarioLoginSerializer(data=request.data)
         if serializer.is_valid():
-            correo = serializer.validated_data['correo']
-            password = serializer.validated_data['password']
-
-            # Autenticar al usuario
-            user = authenticate(request, correo=correo, password=password)
-
-            if user:
+            user = authenticate(request, correo=serializer.validated_data['correo'], password=serializer.validated_data['password'])
+            if user and check_password(serializer.validated_data['password'], user.password):
                 print('Usuario autenticado')
                 login(request, user)
-                return JsonResponse({'message': 'Inicio de sesión exitoso'})
+
+                # Generar tokens
+                refresh = RefreshToken.for_user(user)
+                access_token = str(refresh.access_token)
+                refresh_token = str(refresh)
+
+                return Response({
+                    'message': 'Inicio de sesión exitoso',
+                    'refresh': refresh_token,
+                    'access': access_token,
+                })
             else:
                 print('Credenciales inválidas')
-                return JsonResponse({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+                return Response({'error': 'Credenciales inválidas'}, status=401)
+        return Response(serializer.errors, status=400)
 
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RecuperacionContrasena(APIView):
     # Implementa la lógica de recuperación de contraseña
     pass
 
+
+
+
 class AdminListCreateView(generics.ListCreateAPIView):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
 
+
 class AdminDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
+
 
 class AdminLoginView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -69,6 +81,7 @@ class AdminLoginView(APIView):
             return Response({'token': token.key})
         else:
             return Response({'error': 'Credenciales inválidas'}, status=401)
+
 
 class AdminLogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]

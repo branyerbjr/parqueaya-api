132a5e84c00948384675cd9ef229308e275aadd6
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-12-14 16:38:21 -0500

    cmabio

--- apiadmin/serializers.py
+++ apiadmin/serializers.py
@@ -10,4 +10,4 @@ class AdminSerializer(serializers.ModelSerializer):
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['id', 'telefono', 'nombres', 'apellidos', 'dni', 'correo', 'fecha_registro', 'photo_url']
+        fields = '__all__'

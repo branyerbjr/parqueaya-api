ea857143fbd523b4e9eb63d326cf4e0c9ec897c2
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-12-14 23:23:11 -0500

    cambio aute

--- api_admin/settings.py
+++ api_admin/settings.py
@@ -85,7 +85,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
 }

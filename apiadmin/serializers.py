deae26082961b185e88292cab10a5db61623b8f6
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-12-15 01:09:13 -0500

    cambio aute

--- apiadmin/serializers.py
+++ apiadmin/serializers.py
@@ -11,3 +11,8 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+class UsuarioLoginSerializer(serializers.Serializer):
+    correo = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'})
+

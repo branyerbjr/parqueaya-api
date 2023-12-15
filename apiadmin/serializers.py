306aa7f8edfb8462cd4a6424cff3d4da093b0465
CarlosDaniel0293 <carlos.mendoza.co@tecsup.edu.pe>
2023-12-15 01:11:51 -0500

    cambio aute

--- apiadmin/serializers.py
+++ apiadmin/serializers.py
@@ -10,9 +10,22 @@ class AdminSerializer(serializers.ModelSerializer):
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['id', 'provider_id', 'provider_specific_uid', 'nombre', 'apellidos', 'dni', 'telefono', 'correo', 'photo_url', 'password']
+
+
+class UsuarioRegistrationSerializer(serializers.ModelSerializer):
+    contrasena = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Usuario
+        fields = ['id', 'provider_id', 'provider_specific_uid', 'nombre', 'dni', 'correo', 'photo_url', 'password']
+
+    def create(self, validated_data):
+        user = Usuario.objects.create_user(**validated_data)
+        return user
+
 
 class UsuarioLoginSerializer(serializers.Serializer):
     correo = serializers.EmailField()
-    password = serializers.CharField(style={'input_type': 'password'})
+    contrasena = serializers.CharField(style={'input_type': 'password'})
 
